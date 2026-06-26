1fb0e9a38cc84abe1ddebc055e043cca79a96a3f
Tumansh <vijtumansh06@gmail.com>
2026-06-27 03:14:52 +0530

    Add Redis caching with Cache-Aside pattern

--- src/main/java/com/tumansh/shortlink/controller/TestController.java
+++ src/main/java/com/tumansh/shortlink/controller/TestController.java
@@ -1,34 +1,42 @@
-package com.tumansh.shortlink.controller;
 
-import com.tumansh.shortlink.security.JWTService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/test")
-public class TestController {
 
-    private final JWTService jwtService;
+// Just Implementation to check about JWT working
 
-    public TestController(
-            JWTService jwtService) {
+//package com.tumansh.shortlink.controller;
 
-        this.jwtService = jwtService;
-    }
 
-    @GetMapping("/email")
-    public String getEmail(
-            @RequestParam String token) {
 
-        return jwtService.extractEmail(token);
-    }
-
-    @GetMapping("/validate")
-    public Boolean validate(
-            @RequestParam String token) {
-
-        return jwtService.isTokenValid(token);
-    }
-}
+//
+//import com.tumansh.shortlink.security.JWTService;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/test")
+//public class TestController {
+//
+//    private final JWTService jwtService;
+//
+//    public TestController(
+//            JWTService jwtService) {
+//
+//        this.jwtService = jwtService;
+//    }
+//
+//    @GetMapping("/email")
+//    public String getEmail(
+//            @RequestParam String token) {
+//
+//        return jwtService.extractEmail(token);
+//    }
+//
+//    @GetMapping("/validate")
+//    public Boolean validate(
+//            @RequestParam String token) {
+//
+//        return jwtService.isTokenValid(token);
+//    }
+//}
---
+++ src/main/java/com/tumansh/shortlink/dto/cache/CachedUrlDto.java
@@ -0,0 +1,39 @@
+//This I would use during extension of this project
+
+//package com.tumansh.shortlink.dto.cache;
+//
+//public class CachedUrlDto {
+//    private static final long serialVersionUID = 1L;
+//    private Long id;
+//
+//    private String originalUrl;
+//
+//    public CachedUrlDto() {
+//    }
+//
+//    public CachedUrlDto(
+//            Long id,
+//            String originalUrl) {
+//
+//        this.id = id;
+//        this.originalUrl = originalUrl;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(
+//            Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getOriginalUrl() {
+//        return originalUrl;
+//    }
+//
+//    public void setOriginalUrl(
+//            String originalUrl) {
+//        this.originalUrl = originalUrl;
+//    }
+//}
